57e8e579a56c36aa4fdb2ab2c9b94e8fbfb421a1
Piotr Bechcicki <piotr.bechcicki.o@gmail.com>
2020-03-22 20:30:34 +0100

    Fixed linter warnings.

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,27 +1,46 @@
 import React from 'react';
 import { Container, Header, View, Button, Icon, Fab } from 'native-base';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1
+  },
+  fab: {
+    backgroundColor: '#5067FF'
+  },
+  whatsapp: {
+    backgroundColor: '#34A34F'
+  },
+  facebook: {
+    backgroundColor: '#3B5998'
+  },
+  mail: {
+    backgroundColor: '#DD5144'
+  }
+});
 
 export default () => {
   const [active, setActive] = React.useState(false);
   return (
     <Container>
       <Header />
-      <View style={{ flex: 1 }}>
+      <View style={styles.view}>
         <Fab
           active={active}
           direction="up"
-          style={{ backgroundColor: '#5067FF' }}
+          style={styles.fab}
           position="bottomRight"
           onPress={() => setActive(!active)}
         >
           <Icon name="share" />
-          <Button style={{ backgroundColor: '#34A34F' }}>
+          <Button style={styles.whatsapp}>
             <Icon name="logo-whatsapp" />
           </Button>
-          <Button style={{ backgroundColor: '#3B5998' }}>
+          <Button style={styles.facebook}>
             <Icon name="logo-facebook" />
           </Button>
-          <Button disabled style={{ backgroundColor: '#DD5144' }}>
+          <Button disabled style={styles.mail}>
             <Icon name="mail" />
           </Button>
         </Fab>
