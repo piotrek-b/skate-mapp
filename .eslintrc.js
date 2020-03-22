b30de7a900058709e49f677c12560b16faef88da
Piotr Bechcicki <piotr.bechcicki.o@gmail.com>
2020-03-22 19:05:01 +0100

    Configured ESLint and prettier.

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'airbnb-typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react'
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    project: 'tsconfig.json',
+  }
+};
