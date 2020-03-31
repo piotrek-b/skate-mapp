cea0623d87509929d54effaaae00a7f9e2e36b4f
Piotr Bechcicki <piotr.bechcicki.o@gmail.com>
2020-03-31 23:44:18 +0200

    Added location button (design only).

--- src/FabButton.tsx
+++ src/FabButton.tsx
@@ -9,6 +9,14 @@ const styles = StyleSheet.create({
   fab: {
     backgroundColor: '#5067FF',
   },
+  locationButton: {
+    backgroundColor: '#fff',
+    bottom: 70,
+    position: 'absolute',
+  },
+  locationButtonIcon: {
+    color: '#888',
+  },
   whatsapp: {
     backgroundColor: '#34A34F',
   },
@@ -27,23 +35,40 @@ interface IFabButtonProps {
 export default ({ onClick }: IFabButtonProps) => {
   const [active, setActive] = useState(false);
   return (
-    <Fab
-      active={active}
-      direction="up"
-      style={styles.fab}
-      position="bottomRight"
-      onPress={() => setActive(!active)}
-    >
-      <Icon name="star" />
-      <Button onPress={onClick} style={styles.whatsapp}>
-        <Icon name="logo-whatsapp" />
-      </Button>
-      <Button style={styles.facebook}>
-        <Icon name="logo-facebook" />
-      </Button>
-      <Button disabled style={styles.mail}>
-        <Icon name="mail" />
-      </Button>
-    </Fab>
+    <>
+      <Fab
+        active={active}
+        direction="up"
+        style={styles.fab}
+        position="bottomRight"
+        onPress={() => setActive(!active)}
+      >
+        <Icon name="star" />
+        <Button onPress={onClick} style={styles.whatsapp}>
+          <Icon name="logo-whatsapp" />
+        </Button>
+        <Button style={styles.facebook}>
+          <Icon name="logo-facebook" />
+        </Button>
+        <Button disabled style={styles.mail}>
+          <Icon name="mail" />
+        </Button>
+      </Fab>
+      {!active && (
+        <Fab
+          active={false}
+          direction="up"
+          style={styles.locationButton}
+          position="bottomRight"
+        >
+          <Icon
+            name="md-locate"
+            android="md-locate"
+            ios="ios-locate"
+            style={styles.locationButtonIcon}
+          />
+        </Fab>
+      )}
+    </>
   );
 };
